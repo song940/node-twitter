c7d926d50a0bb1cb7e7cbca463641bb71854ee2f
Lsong <song940@gmail.com>
2016-11-24 23:32:27 +0800

    modify

--- example/index.js
+++ example/index.js
@@ -1,20 +1,18 @@
 const Twitter = require('..');
 
 var twitter = new Twitter({
-  consumer_key       : 'Iy6TIRxgqaGYW6QA0qNU2w',
-  consumer_secret    : 'BlNfIqyxTyfSRpUomnCQ87JbpfAPDxDyL0Qcq3yBE',
-  access_token_key   : '194524135-RPv0yP3MGiASsfLh7KvcA0rZ17wJLvKShcuyShbt',
-  access_token_secret: 'qPo8YPGkDL6WyuBlPCuAiV5nDrHwGTijEUqJb09scyeg5'
+  consumer_key   : 'Iy6TIRxgqaGYW6QA0qNU2w',
+  consumer_secret: 'BlNfIqyxTyfSRpUomnCQ87JbpfAPDxDyL0Qcq3yBE'
 });
-// 
-// twitter.search('keyword', {}, function(err, tweets){
-//   
-// });
-// 
-// twitter.post('hello twitter!', function(err, res){
-//   
-// });
 
-twitter.timeline('song940', function(err, tweets){
-  console.log(tweets);
+twitter.token(function(err, token){
+  twitter.invalidate(token.access_token, function(err, token){
+    console.log(token)
+  });
 });
+
+
+
+// twitter.timeline('twitterapi', function(err, tweets){
+//   console.log(tweets);
+// });
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const qs    = require('querystring');
+const http = require('http');
 const https = require('https');
 const pkg   = require('./package');
 
@@ -9,33 +11,59 @@ function Twitter(options){
   if(!(this instanceof Twitter))
     return new Twitter(options);
   var defaults = {
-    endpoint: 'api.twitter.com',
-    version: '1.1'
+    api: 'api.twitter.com',
   };
   for(var key in options){
     defaults[ key ] = options[ key ];
   }
   this.options = defaults;
   return this;
-}
+};
 
-Twitter.prototype.request = function(method, path, params, callback){
+Twitter.REST = '/1.1';
+
+Twitter.prototype.request = function(method, path, query, header, callback){
+  method = method.toUpperCase();
+  header = header || {};
+  query  = query  || {};
   // https://dev.twitter.com/oauth/application-only
-  var api     = this.options.endpoint;
-  var version = this.options.version;
   var options = {
     method  : method,
-    hostname: api,
-    path    : '/' + version + path,
+    hostname: this.options.api,
+    // hostname: 'localhost',
+    // port: 8080,
+    path    : path,
     headers : {
-      Accept      : '*/*',
-      Connection  : 'close',
-      'User-Agent': [ pkg.name, pkg.version ].join('/')
+      Connection    : 'close',
+      'User-Agent'  : [ pkg.name, pkg.version ].join('/'),
     }
   };
-  if(method === 'get'){
-    options.qs = params;
+  
+  if(this.options.bearer){
+    var bearer = new Buffer(this.options.bearer).toString('base64');
+    options.headers[ 'Authorization' ] = 'Bearer ' + bearer;
   }
+
+  for(var k in header) 
+    options.headers[ k ] = header[k];
+  var body = '';
+  if(method === 'GET'){
+    options.path += '?' + qs.stringify(query);
+  }
+  if(method === 'POST'){
+    options.headers['Content-Type'] = 
+    options.headers['Content-Type'] || 'application/x-www-form-urlencoded';
+    var type = (options.headers['Content-Type']).split(/;\s*/)[0];
+    switch(type){
+      case 'application/json':
+        body = JSON.stringify(query); 
+        break;
+      case 'application/x-www-form-urlencoded': 
+        body = qs.stringify(query); 
+        break;
+    }
+  }
+  console.log(options);
   var req = https.request(options, function(res){
     var buffer = '';
     res
@@ -44,20 +72,58 @@ Twitter.prototype.request = function(method, path, params, callback){
       buffer += chunk;
     })
     .on('end', function(){
+      console.log(buffer, body);
       callback(null, JSON.parse(buffer));
     });
   }.bind(this));
-  req.end();
+  req.end(body);
   return this;
 };
 
-Twitter.prototype.search = function () {
-  
+/**
+ * [get description]
+ * @param  {[type]}   path     [description]
+ * @param  {[type]}   params   [description]
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
+Twitter.prototype.get = function(path, params, callback){
+  this.token(function(err, token){
+    this.options.bearer = token.access_token;
+    return this.request('GET', path, params, {}, callback);
+  }.bind(this));
 };
+
+Twitter.prototype.token = function(callback){
+  if(this.options.bearer)
+    return callback(null, { access_token: this.options.bearer });
+  return this.request('POST', '/oauth2/token', {
+    grant_type: 'client_credentials'
+  }, {
+    Authorization: 'Basic ' + new Buffer([ 
+      this.options.consumer_key, 
+      this.options.consumer_secret 
+    ].join(':')).toString('base64')
+  }, callback);
+};
+
+
+Twitter.prototype.invalidate = function(token, callback){
+  return this.request('POST', '/oauth2/invalidate_token', {
+    access_token: token
+  }, {
+    Authorization: 'Basic ' + new Buffer([ 
+      this.options.consumer_key, 
+      this.options.consumer_secret 
+    ].join(':')).toString('base64')
+  }, callback);
+};
+
+
 
 Twitter.prototype.timeline = function (params, callback){
   if(typeof params === 'string') params = { screen_name: params };
-  return this.request('GET', '/statuses/user_timeline', params, callback);
+  return this.get('/1.1/statuses/user_timeline', params, callback);
 };
 
 Twitter.prototype.user = function () {
